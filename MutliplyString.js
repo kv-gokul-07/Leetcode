17187ac240cabd4e0e51c28560c1cce72f72a2a6
gokul-goodux <gokulkv42@gmail.com>
2025-06-14 00:07:52 +0530

    multiplyString

---
+++ MutliplyString.js
@@ -0,0 +1,9 @@
+// Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+// Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+var multiply = function(num1, num2) {
+    return "" + num1 * num2
+};
+
+console.log(multiply("3", "3"));
